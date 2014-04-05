5624623d1ab54bae65f69dfe20709b349d19890e
U10216007 <qaz13679@yahoo.com>
2014-04-05 15:43:11 +0800

    完成
    1.將原本直接add(new
    JTextField(3))改為先建立物件在add,因為在下面需要用到TextField的名稱去把使用者所輸入的值拿來運算
    2.將原本直接add(new
    JButton("calculate"));改為先建立物件在add,因為必須要增加jbt.addActionListener(new
    ButtonListner());讓button作用,所以button需要有名稱
    3.增加了一個listener class和 actionPerformed
    method來寫當button按下去時會如何作用,也把作用的內容寫在此class,method裡
    4.在listner class的actionPerformed method中增加
    double height = Double.parseDouble(jheight.getText());
    double weight = Double.parseDouble(jweight.getText());
    去取得使用者所輸入的身高體重
    5.寫一個getBMI method 去利用取得的身高體重算出bmi
    6.寫一個getStatus method 再利用if-else 來判斷使用者的status
    7.增加兩個TextField,label要將bmi值和status顯示出來
    8.在listner class的actionPerformed method中增加
    jbmi.setText(String.format("%2f",getBMI(weight,height)));
    jstatus.setText(String.format(getStatus(weight,height)));
    去把計算好的bmi值和status放進jbm, jstatus這兩個TextField裡面
    9. 因為有利用到listner class因此需要增加import java.awt.event.*;
    10.將frame的size加大

--- src/UseBMIClass.java
+++ src/UseBMIClass.java
@@ -1,29 +1,78 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 public class UseBMIClass extends JFrame{
+
+	
+	private JTextField jheight = new JTextField(8);
+	private JTextField jweight = new JTextField(8);
+	private JButton jbt = new JButton("calculate");
+	private JTextField jbmi = new JTextField(8);
+	private JTextField jstatus = new JTextField(8);
+	
 	
 	public UseBMIClass(){
 		
-
 		setLayout(new GridLayout(0,2,0,0));
 		
 		add(new JLabel("name"));
 		add(new JTextField(8));
 		add(new JLabel("age"));
-		add(new JTextField(3));
+		add(new JTextField(8));
 		add(new JLabel("height(cm)"));
-		add(new JTextField(8));
+		add(jheight);
 		add(new JLabel("weight(kg)"));
-		add(new JTextField(8));
-		add(new JButton("calculate"));
+		add(jweight);
+		add(new JLabel("BMI"));
+		add(jbmi);
+		add(new JLabel("status"));
+		add(jstatus);
+		add(jbt);
+
+		
+		
+	    jbt.addActionListener(new  ButtonListner());
+	
 	}
+	
+	class ButtonListner implements ActionListener{
+		public void actionPerformed(ActionEvent e){
+			 double height = Double.parseDouble(jheight.getText());
+			 double weight = Double.parseDouble(jweight.getText());
+			 jbmi.setText(String.format("%.2f",getBMI(weight,height)));
+			 jstatus.setText(String.format(getStatus(weight,height)));
+			}
+
+		public double getBMI(double weight,double height){
+			double bmi = weight / ((height/100)*(height/100));
+			return bmi;
+		}
+		
+		public String getStatus(double weight,double height){
+			double bmi = weight / ((height/100)*(height/100));
+			if(bmi < 18.5)
+				return "underweight";
+			else if (bmi < 25)
+				return "Normal";
+			else if(bmi < 30)
+				return "overweight";
+			else
+				return "Obese";
+			
+		}
+		
+		
+		
+	}
+	
+	
 	
 	public static void main(String[] args){
 		
 		UseBMIClass frame = new UseBMIClass();
 		frame.setTitle("BMI");
-		frame.setSize(200,150);
+		frame.setSize(300,200);
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
@@ -34,3 +83,5 @@ public class UseBMIClass extends JFrame{
 	
 
 }
+
+
